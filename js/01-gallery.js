2800a36de6980ab2629ae4aeedf2a05c606889d5
Grek Maria <grekmariaa@gmail.com>
2022-08-09 17:37:10 +0300

    Image gallery(task 01) is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryItem(galleryItem) {
+    const { preview, original, description } = galleryItem;
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+</div>`;
+}
+
+const createGalleryItems = galleryItems.map(createGalleryItem).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", createGalleryItems);
+
+function callModalWithLargeImage(event) {
+    event.preventDefault();
+    const sourseImage = event.target.dataset.source;
+    console.log('~ sourseImage', sourseImage)
+    return basicLightbox.create(`
+<img src='${sourseImage}' width="800" height="600">
+`).show()
+}
+
+galleryEl.addEventListener("click", callModalWithLargeImage);
+
